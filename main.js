a14292de5e0aff56b33cd5e39f50173aa30cf5a5
Kou1029 <huangkisiwan@gmail.com>
2024-12-05 19:04:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -43,88 +43,84 @@ const token = new SkyWayAuthToken({
 }).encode("WycaGzghuM+aTaX/MPXfe9gP5ayoc9S47PjyDBm+SB4=");
 
 (async () => {
-    const localVideo = document.getElementById("local-video");
-    const buttonArea = document.getElementById("button-area");
-    const remoteMediaArea = document.getElementById("remote-media-area");
-    const roomNameInput = document.getElementById("room-name");
-    // document.getElementById('room-name').value = "a";
+  const localVideo = document.getElementById("local-video");
+  const buttonArea = document.getElementById("button-area");
+  const remoteMediaArea = document.getElementById("remote-media-area");
+  const roomNameInput = document.getElementById("room-name");
 
-    const myId = document.getElementById("my-id");
-    const joinButton = document.getElementById("join");
-    const leaveButton = document.getElementById("leave");
+  const myId = document.getElementById("my-id");
+  const joinButton = document.getElementById("join");
+  const leaveButton = document.getElementById("leave");
 
-    const { audio, video } =
-        await SkyWayStreamFactory.createMicrophoneAudioAndCameraStream();
-    video.attach(localVideo);
-    await localVideo.play();
+  const { audio, video } =
+    await SkyWayStreamFactory.createMicrophoneAudioAndCameraStream();
+  video.attach(localVideo);
+  await localVideo.play();
 
+  joinButton.onclick = async () => {
     if (roomNameInput.value === "") return;
 
     const context = await SkyWayContext.Create(token);
     const room = await SkyWayRoom.FindOrCreate(context, {
-        type: "sfu",
-        name: roomNameInput.value,
+      type: "p2p",
+      name: roomNameInput.value,
     });
     const me = await room.join();
 
-    myId.value = me.id;
+    myId.textContent = me.id;
 
     await me.publish(audio);
     await me.publish(video);
 
     const subscribeAndAttach = (publication) => {
-        if (publication.publisher.id === me.id) return;
+      if (publication.publisher.id === me.id) return;
 
-        const subscribeButton = document.createElement("button");
-        subscribeButton.id = `subscribe-button-${publication.id}`;
-        subscribeButton.textContent = `${publication.publisher.id}: ${publication.contentType}`;
-        buttonArea.appendChild(subscribeButton);
+      const subscribeButton = document.createElement("button");
+      subscribeButton.id = `subscribe-button-${publication.id}`;
+      subscribeButton.textContent = `${publication.publisher.id}: ${publication.contentType}`;
+      buttonArea.appendChild(subscribeButton);
 
-        subscribeButton.onclick = async () => {
-            const { stream } = await me.subscribe(publication.id);
+      subscribeButton.onclick = async () => {
+        const { stream } = await me.subscribe(publication.id);
 
-            let newMedia;
-            switch (stream.track.kind) {
-                case "video":
-                    newMedia = document.createElement("video");
-                    newMedia.playsInline = true;
-                    newMedia.autoplay = true;
-                    break;
-                case "audio":
-                    newMedia = document.createElement("audio");
-                    newMedia.controls = true;
-                    newMedia.autoplay = true;
-                    break;
-                default:
-                    return;
-            }
-            newMedia.id = `media-${publication.id}`;
-            stream.attach(newMedia);
-            remoteMediaArea.appendChild(newMedia);
-        };
+        let newMedia;
+        switch (stream.track.kind) {
+          case "video":
+            newMedia = document.createElement("video");
+            newMedia.playsInline = true;
+            newMedia.autoplay = true;
+            break;
+          case "audio":
+            newMedia = document.createElement("audio");
+            newMedia.controls = true;
+            newMedia.autoplay = true;
+            break;
+          default:
+            return;
+        }
+        newMedia.id = `media-${publication.id}`;
+        stream.attach(newMedia);
+        remoteMediaArea.appendChild(newMedia);
+      };
     };
 
     room.publications.forEach(subscribeAndAttach);
     room.onStreamPublished.add((e) => subscribeAndAttach(e.publication));
 
     leaveButton.onclick = async () => {
-        await me.leave();
-        await room.dispose();
+      await me.leave();
+      await room.dispose();
 
-        myId.textContent = "";
-        buttonArea.replaceChildren();
-        remoteMediaArea.replaceChildren();
-        room.onStreamUnpublished.add((e) => {
-            document.getElementById(`subscribe-button-${e.publication.id}`)?.remove();
-            document.getElementById(`media-${e.publication.id}`)?.remove();
-        });
-        window.location.href="room_in.html";
+      myId.textContent = "";
+      buttonArea.replaceChildren();
+      remoteMediaArea.replaceChildren();
     };
 
-    
-
-    
-
+    room.onStreamUnpublished.add((e) => {
+      document.getElementById(`subscribe-button-${e.publication.id}`)?.remove();
+      document.getElementById(`media-${e.publication.id}`)?.remove();
+    });
+  };
 })();
 
 
