89ed62e567b37152be242e077babf97f3e17f159
Kou1029 <huangkisiwan@gmail.com>
2024-12-09 19:04:48 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ let audio_flag = true;
         async function video_hyouji() {
             // subscribeButton.onclick = async () => {
                 const { stream } = me.subscribe(publication.id);
-                console.log(publication.id);
+                console.log(me.subscribe(publication.id));
                 let newMedia;
                 switch (stream.track.kind) {
                     case "video":
